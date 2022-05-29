51dab9cc1fbc9bd95aee14bf066dec259d9314eb
johnc13b <100248387+johnc13b@users.noreply.github.com>
2022-05-29 01:40:15 -0400

    Create Candidate

--- server.js
+++ server.js
@@ -18,20 +18,32 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-// // **GET a SINGLE candidate** //
-// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(row);
-// });
+// **GET a SINGLE candidate** //
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});
 
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);
-// });
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // **DELETE candidate** //
 db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
+// **CREATE candidate** //
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+                VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
     if (err) {
         console.log(err);
     }
