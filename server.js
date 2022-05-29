d774e0a489cae39b144305b9816b5b01cf1525ba
johnc13b <100248387+johnc13b@users.noreply.github.com>
2022-05-29 01:20:50 -0400

    Select and Delete

--- server.js
+++ server.js
@@ -4,22 +4,38 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Middleware //
+// **Middleware** //
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 const db = mysql.createConnection(
     {
-        host: 'localhost', // use 127.0.0.1
+        host: 'localhost', // use 127.0.0.1 //
         user: 'root',
-        password: 'AtehAthena0430!', // remember to delete
+        password: 'AtehAthena0430!', // remember to delete //
         database: 'election',
     },
     console.log('Connected to the election database.')
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
+// // **GET a SINGLE candidate** //
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// **DELETE candidate** //
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
 });
 
 app.use((request, response) => {
