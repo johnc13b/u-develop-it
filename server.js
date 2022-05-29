f583896693ed0c58dc80a4b67a5ae8f58d2abbed
johnc13b <100248387+johnc13b@users.noreply.github.com>
2022-05-29 01:09:53 -0400

    Database Query

--- server.js
+++ server.js
@@ -12,11 +12,15 @@ const db = mysql.createConnection(
     {
         host: 'localhost', // use 127.0.0.1
         user: 'root',
-        password: 'AtehAthena0430!',
+        password: 'AtehAthena0430!', // remember to delete
         database: 'election',
     },
     console.log('Connected to the election database.')
 );
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 app.use((request, response) => {
     response.status(404).end();
