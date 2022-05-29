73fbe6bc805a163c96414e23ba9d66ab4b0f62fe
johnc13b <100248387+johnc13b@users.noreply.github.com>
2022-05-29 01:01:51 -0400

    Create Connection in Server.JS

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
+const mysql = require('mysql2');
 const { response } = require('express');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware //
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (request, response) => {
-    response.json({
-        message: 'Hello World'
-    });
-});
+const db = mysql.createConnection(
+    {
+        host: 'localhost', // use 127.0.0.1
+        user: 'root',
+        password: 'AtehAthena0430!',
+        database: 'election',
+    },
+    console.log('Connected to the election database.')
+);
 
 app.use((request, response) => {
     response.status(404).end();
